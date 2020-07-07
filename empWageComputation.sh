420b108ee2deb73799e107411b072283d269bf39
swathireddyy <swathithillana12@gmail.com>
2020-07-07 19:10:39 +0530

    Storing the day and daily wage along with total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,29 +1,31 @@
 #!/bin/bash -x
 isPartTime=1
 isFullTime=2
+absent=0
 EmpWagePerHour=20
 numWorkingDays=20
 DailyWage=0
+DailyWages=0
 totalWage=0
-function getWorkHourofEmployee(){
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
 	EmpAttendance=$((RANDOM%3))
 	case $EmpAttendance in
-		$isPartTime)
+		$isPartTime )
 			EmpHours=4
 			;;
-		$isFullTime)
+		$isFullTime )
 			EmpHours=8
 			;;
 		*)
+			EmpHours=0
 			;;
 	esac
-	echo $EmpHours
-}
-getWorkHourofEmployee
-EmpAttendance=$((RANDOM%2))
-DailyWage=$(($EmpWagePerHour*$EmpHours))
-totalWage=$(($DailyWage*$numWorkingDays))
-echo "daily wage of employee is:"$DailyWage
+	DailyWage=$(($EmpWagePerHour*$EmpHours))
+	echo "daily wage of employee in day" $day "is:" $DailyWage
+	DailyWages=$DailyWages+$DailyWage
+done
+totalWage=$(($DailyWages*$numWorkingDays))
 echo "Total wage of employee is:"$totalWage
 
 
