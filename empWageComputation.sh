3fb691a61d56f72dde6cc224c84bf90dee737476
swathireddyy <swathithillana12@gmail.com>
2020-07-07 16:53:19 +0530

    calculating wages till num of working days is reached

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,25 +1,23 @@
 #!/bin/bash -x
 isPartTime=1
 isFullTime=2
-Absent=0
 EmpWagePerHour=20
 numWorkingDays=20
 totalWage=0
-EmpAttendance=$((RANDOM%3))
-case $EmpAttendance in
-	$isPartTime)
-		EmpHours=4
-		;;
-	$isFullTime)
-		EmpHours=8
-		;;
-	*)
-		EmpHours=0
-		;;
-esac
+for (( day=1; day<=$numWorkingDays; day++ ))
+	do
+	EmpAttendance=$((RANDOM%3))
+	case $EmpAttendance in
+		$isPartTime)
+			EmpHours=4
+			;;
+		$isFullTime)
+			EmpHours=8
+			;;
+		*)
+			;;
+	esac
 DailyWage=$(($EmpWagePerHour*$EmpHours))
-echo "Daily wage of the employee is:" $DailyWage
-totalWage=$(($DailyWage*$numWorkingDays))
-echo "Total wage for a month is:" $totalWage
-
+echo "Daily wage for day" $day":" $DailyWage
+done
 
